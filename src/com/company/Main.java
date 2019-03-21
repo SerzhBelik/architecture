7b55f6138eb8995b3b579a6cc260e65b20498ade
Belikov <Sergeybelik86@gmail.com>
2019-03-21 18:15:16 +0300

    завершено

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+
         Controller controller = new ClientController();
         ClientUI clientUI = MainWindow.newBuilder()
                 .setController(controller)
